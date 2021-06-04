e78ada510ee74dd805e428f80ca2c8fcede744ef
Luke Prosser <lukesprosser@gmail.com>
2021-06-04 21:17:01 +0100

    create separate components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const TASKS = [
+  { id: "todo-1", name: "Eat", completed: true },
+  { id: "todo-2", name: "Sleep", completed: false },
+  { id: "todo-3", name: "Repeat", completed: false },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App subject='Luke' />
+    <App tasks={TASKS} />
   </React.StrictMode>,
   document.getElementById('root')
 );
